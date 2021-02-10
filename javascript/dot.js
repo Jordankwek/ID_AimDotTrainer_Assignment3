53a12f187c74d18c6622136e2b9a95f113130a61
Jordankwek <kwekjordan@gmail.com>
2021-02-11 00:19:48 +0800

    Added a javascript to randomize the location of the aim dot upon user's click. Linked the "Play games" in the navigation bar to the blitz game mode for now. Will make changes when more modes are being created.

---
+++ javascript/dot.js
@@ -0,0 +1,18 @@
+dot = $('#aim');
+
+var points = 0;
+
+//From stackoverflow
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+dot.on('click', function(){
+    var vertical = randomInteger(10, 93) + "%";
+    var horizontal = randomInteger(0, 95) + "%";
+    dot.css("top", vertical);
+    dot.css("left", horizontal);
+    points += 1;
+})
+
+$("html").css("user-select", none);
