560f808a82c155435fd39a01e063824ec8aa5a8d
jarrellsoh <s10205047@connect.np.edu.sg>
2021-02-18 12:22:49 +0800

    Added animation to contactus page

---
+++ javascript/contactpage.js
@@ -0,0 +1,25 @@
+var checkbug = 1;
+var checkcontact = 1;
+
+$('#bug-form').on('click', function (event) {
+    if (checkbug % 2 != 0)
+        $('#bugformitem').css("height", "38vh");
+    checkbug += 1;
+})
+
+$('#bugexit').on('click', function (event) {
+    $('#bugformitem').css("height", "0");
+    checkbug = 0;
+})
+
+$('#contact-form').on('click', function (event) {
+    if (checkcontact % 2 != 0)
+        $('#contactformitem').css("height", "43vh");
+    checkcontact += 1;
+})
+
+
+$('#contactexit').on('click', function (event) {
+    $('#contactformitem').css("height", "0");
+    checkcontact = 0;
+})
