4523fd5151f248f20931318b75be2bc43b56f095
Jordankwek <kwekjordan@gmail.com>
2021-02-19 01:24:06 +0800

    Saved the background when the user switches their background. It is saved using local storage.

--- javascript/changebg.js
+++ javascript/changebg.js
@@ -1,10 +1,27 @@
+var web
+
+
 function changeBg(){
     if ($('#websitebackground').attr('src') === 'Images/websitebg.jpg')
     {
         $('#websitebackground').attr("src","Images/altwebsitebg.jpg");
+        localStorage.setItem("background", 'Images/altwebsitebg.jpg');
     }
     else
     {
         $('#websitebackground').attr("src","Images/websitebg.jpg");
+        localStorage.setItem("background", 'Images/websitebg.jpg');
+    }
+}
+
+function checkBg(){
+    var bgLink = localStorage.getItem("background");
+    if (bgLink === 'Images/websitebg.jpg')
+    {
+        $('#websitebackground').attr("src","Images/websitebg.jpg");
+    }
+    else if (bgLink === 'Images/altwebsitebg.jpg')
+    {
+        $('#websitebackground').attr("src","Images/altwebsitebg.jpg");
     }
 }
