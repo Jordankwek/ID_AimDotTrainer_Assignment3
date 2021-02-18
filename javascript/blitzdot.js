155f6d5f7e69c1ce2d1acb7761e0f8909f9dc428
Jordankwek <kwekjordan@gmail.com>
2021-02-18 23:49:10 +0800

    Testing out restdb and adding a dark mode to the website

--- javascript/blitzdot.js
+++ javascript/blitzdot.js
@@ -39,9 +39,11 @@ dot.on('click', function (event) {
         clearInterval(timer);
         dot.hide();
         let post = `
-          <div class = "record" style="position: absolute; text-alight: centre; top: 45%">
+          <div class = "record" style="position: absolute; text-alight: centre; top: 45%; z-index: 2">
             <h1>Game Ended</h1>
             <h5>Points earnt in 30 seconds : ${points}</h5>
+            <input type="text" id="pname" placeholder="Enter your name" required />
+            <input id = "blitzsubmit" type="submit" value="Submit">
           </div>
         `;
         container.insertAdjacentHTML('afterbegin',post);
@@ -51,3 +53,34 @@ dot.on('click', function (event) {
 
 })
 
+$(document).ready(function (){
+  const APIKEY = "57e086546f4c13fec6c77d472c74f155d9145";
+  $("#blitzsubmit").on("click",function(e){
+    e.preventDefault();
+    let playerName = $("#pname").val();
+
+    let jsondata = {
+      "Name": playerName,
+      "Points": points,
+    }
+
+    let settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://aimdottrainer-0c78.restdb.io/rest/blitzinfo",
+      "method": "POST",
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+      },
+      "processData": false,
+      "data": JSON.stringify(jsondata)
+    }
+    
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
+
+  })
+})
---
+++ javascript/darkmode.js
@@ -0,0 +1,4 @@
+function darkMode(){
+    $('html').css("color",'black');
+    $('html').css("background-color",'black');
+}
